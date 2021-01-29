307d8b1da88f7a988d7a5ee6c62c7f28ad35ff4b
ChloeFeng23 <78114513+ChloeFeng23@users.noreply.github.com>
2021-01-29 13:09:26 -0800

    Tic Tac Toe

---
+++ Noughts and Crosses - Start/script.js
@@ -0,0 +1,82 @@
+var currentPlayer = 1;//current player 1 r 2
+var plays = 0; //counts the number of players or turns taken
+
+var board = document.querySelector("#board");//pointer to the form
+
+board.b0.addEventListener("click",takeTurn);
+board.b1.addEventListener("click",takeTurn);
+board.b2.addEventListener("click",takeTurn);
+board.b3.addEventListener("click",takeTurn);
+board.b4.addEventListener("click",takeTurn);
+board.b5.addEventListener("click",takeTurn);
+board.b6.addEventListener("click",takeTurn);
+board.b7.addEventListener("click",takeTurn);
+board.b8.addEventListener("click",takeTurn);
+
+function takeTurn(){
+	if(currentPlayer == 1 && this.value == ""){
+		this.value = "X";
+		this.style.color = "red";
+		currentPlayer = 2;
+		plays++;
+		if(plays >= 5){
+			checkWin();
+		}
+	}
+	else if(currentPlayer == 2 && this.value == ""){
+		this.value = "O";
+		this.style.color = "green";
+		currentPlayer = 1;
+		plays++;
+		if(plays >= 5){
+			checkWin();
+		}
+	}
+	console.log("plays = " + plays);
+}//end takeTurn function
+
+function checkWin(){
+	if(board.b0.value == board.b1.value && board.b1.value == board.b2.value && board.b0.value != ""){
+		announceWin(board.b0.value);
+	}
+	else if(board.b3.value == board.b4.value && board.b4.value == board.b5.value && board.b3.value != ""){
+		announceWin(board.b3.value);
+	}
+	else if(board.b6.value == board.b7.value && board.b7.value == board.b8.value && board.b6.value != ""){
+		announceWin(board.b6.value);
+	}
+	else if(board.b0.value == board.b3.value && board.b3.value == board.b6.value && board.b0.value != ""){
+		announceWin(board.b0.value);
+	}
+	else if(board.b1.value == board.b4.value && board.b4.value == board.b7.value && board.b1.value != ""){
+		announceWin(board.b1.value);
+	}
+	else if(board.b2.value == board.b5.value && board.b5.value == board.b8.value && board.b2.value != ""){
+		announceWin(board.b2.value);
+	}
+	else if(board.b0.value == board.b4.value && board.b4.value == board.b8.value && board.b0.value != ""){
+		announceWin(board.b0.value);
+	}
+	else if(board.b2.value == board.b4.value && board.b4.value == board.b6.value && board.b2.value != ""){
+		announceWin(board.b2.value);
+	}
+	else if(plays == 9){
+		annunceCatsGame();
+	}
+}//end of checkWin
+
+function announceWin(winner){
+	document.querySelector("#popup").innerHTML = '<img src="image/183568.png" alt="x button" id="x"><h1>' + winner + ' Wins!</h1>';
+	document.querySelector("#x").addEventListener("click",hidePopup);
+	document.querySelector("#container").style.display = "flex";
+}
+
+function annunceCatsGame(){
+	document.querySelector("#container").style.display = "flex";
+}
+
+document.querySelector("#x").addEventListener("click",hidePopup);
+
+function hidePopup(){
+	document.querySelector("#container").style.display = "none";
+}
